199316604909cd372650e83ada99237da2fe7f88
YasinHafiz <52492642+YasinHafiz@users.noreply.github.com>
2019-11-19 01:45:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,25 +13,8 @@ function yapilacakListe(){
 		const element = event.target;
 		if(element.type == "checkbox") {
             element.parentNode.style.textDecoration = "line-through";
-        /*    
-            function sleep(milliseconds) {
-                var start = new Date().getTime();
-                for (var i = 0; i < 1e7; i++) {
-                  if ((new Date().getTime() - start) > milliseconds){
-                    break;
-                  }
-                }
-              }
-              sleep(500);
-              var textt= document.getElementById(element.parentNode.text)
-              document.getElementById("yapilacakListe").appendChild(textt)
-              element.parentNode.remove()
-              
-            */
+       
             
-        }
-        else {
-            element.parentNode.style.textDecoration = "line-through";
         }
 	}
     if(item== ""){
